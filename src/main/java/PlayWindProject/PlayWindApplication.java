f35d5dffb33c1d1bfae27dd00968f1dfdd8ffaf9
Fwdhus <Fawad_98@hotmail.co.uk>
2022-03-03 11:24:54 +0000

    Initial commit

---
+++ src/main/java/PlayWindProject/PlayWindApplication.java
@@ -0,0 +1,13 @@
+package PlayWindProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWindApplication.class, args);
+	}
+	
+}
